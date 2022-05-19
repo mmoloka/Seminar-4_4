9e5375086071fb2ab673c6c4337fd621f8486eaa
«Mikhail» <kopfgolova@gmail.com>
2022-05-19 21:12:22 +0300

    add Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120*/
+
+int GetProdOfNum (int num)
+{
+    int prod = 1;
+    for(int i=1;i<=num;i++)
+    prod*=i;
+    return prod;
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int prod = GetProdOfNum(num);
+Console.WriteLine(prod);
