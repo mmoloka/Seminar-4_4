dbd0571ad745b302d58ff627e219edd8c9623afe
«Mikhail» <kopfgolova@gmail.com>
2022-05-19 20:14:04 +0300

    add Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36*/
+bool IsNumberPositive(int number)
+{
+    return number > 0;
+}
+int GetSumFromOneToNumber(int number)
+{
+    int sum = 0;
+    for(int i = 1;i <= number;i++)
+    sum += i;    //sum = sum + i
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if(IsNumberPositive(number))
+{
+    int sum = GetSumFromOneToNumber(number);
+    Console.WriteLine($"Сумма чисел от 1 до n: {sum}");
+}
+else
+{
+    Console.WriteLine("Некорректное число");
+}
