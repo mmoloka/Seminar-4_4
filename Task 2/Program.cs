983490100b74123236447a9652f71d676d30f884
«Mikhail» <kopfgolova@gmail.com>
2022-05-19 20:37:57 +0300

    add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+string GetNumber()
+{
+    return Console.ReadLine();
+}
+int CountNum(string str)                   
+{
+                                           // int num = int.Parse(str);
+                                           // int count = 0;
+                                           // while(num>0)
+  return str.Length;                       // {
+                                           // num = num / 10;
+                                           // count++    
+                                           // }
+}                                          //return count     
+Console.WriteLine("Введите число");
+int countNum = CountNum(GetNumber());
+Console.WriteLine(countNum);
